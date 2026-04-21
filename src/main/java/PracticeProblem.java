8e86eacac54cb03fdd19cd085ff577ae671dc9b4
Peter-Zheng1 <343277125@tdsb.ca>
2026-04-21 15:15:18 +0000

    Done

--- src/main/java/PracticeProblem.java
+++ src/main/java/PracticeProblem.java
@@ -1,7 +1,46 @@
+/**
+
+        * File: Lesson 4.4
+
+        * Author: Peter
+
+        * Date Created: April 21, 2026
+
+        * Date Last Modified: April 21, 2026
+
+        */
+
 public class PracticeProblem {
 
 	public static void main(String args[]) {
 
 	}
 
+
+	public static boolean hasCapital(String word){
+		
+		for(int s = 0; s < word.length(); s++) {
+			if (Character.isUpperCase(word.charAt(s))) {
+				return true;
+			}
+		}
+		return false;
+	}
+
+	public static boolean isPrime(int num) {
+		
+		if (num <= 1) {
+			return false;
+		}
+		
+		for (int s = 2; s < num; s++) {
+			if (num % s == 0){
+				return false;
+			}
+		}
+
+		return true;
+	}
+
+	
 }
